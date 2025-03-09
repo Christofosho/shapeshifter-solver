0f59029192670b9948a076eca0bad9d201921833
Chris <csnow@live.ca>
2025-03-08 20:25:19 -0500

    Naïve DFS

--- solver.js
+++ solver.js
@@ -1,11 +1,5 @@
 // shapeshifter solver stub
 
-// 1. Sort shapes with largest first.
-// 2. Find all placements of the largest shape.
-// 3. For each placement, find all placements of the next largest shape.
-// 4. Repeat until all shapes have been placed.
-// 5. If a solution is found, print it and exit.
-
 console.log(`${new Date().toISOString()} Run begins...`);
 
 const puzzle = require("./puzzle.json");
@@ -28,65 +22,45 @@ for (const i in shapes) {
 
 // Inefficient sort because other things will take longer.
 const sortedShapes = [...shapes].sort((a, b) => {
-  return shapeValues[a.index] - shapeValues[b.index];
+  return shapeValues[b.index] - shapeValues[a.index];
 });
-
-const sortedCopy = [...sortedShapes];
 
 const sequence = puzzle.sequence;
 const seqLength = sequence.length;
 
-function applyShapeToBoard(board, shape, x, y) {
-  const newBoard = board.map(row => row.slice());
+console.log(`${new Date().toISOString()} ${shapes.length} shapes, ${seqLength} sequence, ${board.length}x${board[0].length} board`);
+
+let totalComplexity = 1;
+for (const shape of shapes) {
+  let currentComplexity = 0;
+  for (let y = 0; y <= board.length - shape.shape.length; y++) {
+    for (let x = 0; x <= board[0].length - shape.shape[0].length; x++) {
+      ++currentComplexity;
+    }
+  }
+  totalComplexity *= currentComplexity;
+}
+console.log(`Total complexity without optimizations: ${totalComplexity}`);
+
+function applyShapeToBoard(shape, x, y) {
   for (let i = 0; i < shape.length; ++i) {
     for (let j = 0; j < shape[i].length; ++j) {
       if (shape[i][j] === 1) {
-        newBoard[y + i][x + j] = (newBoard[y + i][x + j] + 1) % seqLength;
+        board[y + i][x + j] = (board[y + i][x + j] + 1) % seqLength;
       }
     }
   }
-  return newBoard;
 }
 
-// All shapes can only be placed at at minimum an offset of their width and height,
-// so we can skip the first few rows and columns and the last few rows and columns.
-// Solution uses a stack to perform a depth-first search.
-// For each piece, the solution tries to place it in every possible position on the board.
-// It saves each possible board state.
-// For each initial board state, the solution tries to place the next piece in every possible position.
-console.log(`${new Date().toISOString()} Applying shapes...`);
-
-// Start with the largest shape
-let boards = [];
-let index = sortedShapes.length - 1;
-let shape = sortedShapes.pop().shape;
-
-for (let y = 0; y <= board.length - shape.length; y++) {
-  for (let x = 0; x <= board[0].length - shape[0].length; x++) {
-    boards.push({
-      board: applyShapeToBoard(board, shape, x, y),
-      xy: [{ index, pos: [x, y]}],
-    });
-  }
-}
-
---index;
-while (sortedShapes.length) {
-  const nextBoards = [];
-  shape = sortedShapes.pop().shape;
-  for (const currentBoard of boards) {
-    for (let y = 0; y <= board.length - shape.length; y++) {
-      for (let x = 0; x <= board[0].length - shape[0].length; x++) {
-        nextBoards.push({
-          board: applyShapeToBoard(currentBoard.board, shape, x, y),
-          xy: currentBoard.xy.concat([{ index, pos: [x, y] }]),
-        });
+function removeShapeFromBoard(shape, x, y) {
+  for (let i = 0; i < shape.length; ++i) {
+    for (let j = 0; j < shape[i].length; ++j) {
+      if (shape[i][j] === 1) {
+        const next = (board[y + i][x + j] - 1);
+        board[y + i][x + j] = next < 0 ? seqLength - 1 : next;
       }
     }
   }
-
-  boards = nextBoards;
-  --index;
 }
 
 // Check if board equals solution
@@ -101,18 +75,78 @@ function isSolution(board) {
   return true;
 }
 
-console.log(`${new Date().toISOString()} Checking solutions...`);
+console.log(`${new Date().toISOString()} Applying shapes...`);
 
-for (const board of boards) {
-  if (isSolution(board.board)) {
-    console.log("Solved:");
-    console.log(board);
-    board.xy.sort((a, b) => sortedCopy[a.index].index - sortedCopy[b.index].index);
-    board.xy.forEach(({ index, pos }) => {
-      console.log(shapes[index].shape, " at ", pos);
-    });
-    break;
+const cache = {};
+
+const stack = [];
+const dive = (shapeIndex, boardX, boardY) => {
+  applyShapeToBoard(sortedShapes[shapeIndex].shape, boardX, boardY);
+  stack.push({ index: sortedShapes[shapeIndex].index, pos: [boardX, boardY] });
+
+  if (shapeIndex < shapes.length - 1) {
+    const nextIndex = shapeIndex + 1;
+    const nextShape = sortedShapes[nextIndex].shape;
+    for (let y = 0; y <= board.length - nextShape.length; y++) {
+      for (let x = 0; x <= board[0].length - nextShape[0].length; x++) {
+        dive(nextIndex, x, y);
+        if (nextIndex + 1 === shapes.length) {
+          if (isSolution(board)) {
+            stack.sort((a, b) => a.index - b.index);
+            stack.forEach(({ index, pos }) => {
+              console.log(pos, "with index", index, "for shape", shapes[index].shape);
+            });
+            throw "done";
+          }
+        }
+        removeShapeFromBoard(nextShape, x, y);
+        stack.pop();
+      }
+    }
   }
 }
+
+for (let y = 0; y <= board.length - sortedShapes[0].shape.length; y++) {
+  for (let x = 0; x <= board[0].length - sortedShapes[0].shape[0].length; x++) {
+    dive(0, x, y);
+    removeShapeFromBoard(sortedShapes[0].shape[0], x, y);
+    stack.pop();
+  }
+}
+
+// const stack = [{ shape: sortedShapes[0], xy: [0, 0] }];
+// while (stack.length) {
+//   const nextBoards = [];
+//   const shape = sortedShapes.pop().shape;
+//   for (const currentBoard of boards) {
+//     for (let y = 0; y <= board.length - shape.length; y++) {
+//       for (let x = 0; x <= board[0].length - shape[0].length; x++) {
+//         nextBoards.push({
+//           board: applyShapeToBoard(currentBoard.board, shape, x, y),
+//           xy: currentBoard.xy.concat([{ index, pos: [x, y] }]),
+//         });
+//       }
+//     }
+//   }
+
+//   boards = nextBoards;
+//   --index;
+// }
+
+
+
+// console.log(`${new Date().toISOString()} Checking solutions...`);
+
+// for (const board of boards) {
+//   if (isSolution(board.board)) {
+//     console.log("Solved:");
+//     console.log(board);
+//     board.xy.sort((a, b) => sortedCopy[a.index].index - sortedCopy[b.index].index);
+//     board.xy.forEach(({ index, pos }) => {
+//       console.log(shapes[index].shape, " at ", pos);
+//     });
+//     break;
+//   }
+// }
 
 console.log(`Run ends at ${new Date().toISOString()}`);
